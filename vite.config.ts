2849d3a59bcbc979ee6aaaad50aa714e2a086ff9
AaronYun <hyensooyoon@gmail.com>
2026-02-04 20:45:50 +0900

    feat(init): 프로젝트 기본 설정 및 의존성 추가

---
+++ vite.config.ts
@@ -0,0 +1,26 @@
+import { resolve } from 'node:path';
+
+import tailwindcss from '@tailwindcss/vite';
+import { TanStackRouterVite } from '@tanstack/router-plugin/vite';
+import react from '@vitejs/plugin-react';
+import { defineConfig } from 'vite';
+
+export default defineConfig({
+  plugins: [
+    TanStackRouterVite({ autoCodeSplitting: true }),
+    react({
+      babel: {
+        plugins: [['babel-plugin-react-compiler']],
+      },
+    }),
+    tailwindcss(),
+  ],
+  resolve: {
+    alias: {
+      '@': resolve(__dirname, './src'),
+    },
+  },
+  server: {
+    port: 3000,
+  },
+});
